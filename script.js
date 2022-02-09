3ae5bdcbfcfb1f09e1020794014638a62502f016
Nathan Lubawski <nathanlubawski1@gmail.com>
2022-02-09 16:24:13 -0300

    criada funcao pra obter acesso ao servidor, criada funcao pra exibir a resposta, esta e gerada na tela de acordo com o tipo da mensagem

---
+++ script.js
@@ -0,0 +1,36 @@
+function obterMensagens(){
+    const promessa = axios.get('https://mock-api.driven.com.br/api/v4/uol/messages')
+    promessa.then(exibe)
+}
+
+function exibe(resposta){
+    console.log(resposta.data)
+    resposta.data.forEach(element => { 
+
+        const textoMain = document.querySelector('.mensagem')
+
+        if (element.type === 'message'){
+
+            textoMain.innerHTML += `
+            <div class="mensagem__publica">
+                <span>${element.time}</span><span>${element.from}  ${element.text}</span>
+            </div>            
+            `
+        } else if (element.type === 'private_message'){
+
+            textoMain.innerHTML += `
+            <div class="mensagem__privada">
+                <span>${element.time}</span><span>${element.from}  ${element.text}</span>
+            </div>
+            `
+        } else if (element.type === 'status') {
+            textoMain.innerHTML += `
+            <div class="mensagem__status">
+                <span>${element.time}</span><span>${element.from}  ${element.text}</span>
+            </div>
+            `
+        }        
+    });
+}
+
+obterMensagens()
