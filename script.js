8f7dc5f1d523727b70b1ac91297782ef7729c012
Nathan Lubawski <nathanlubawski1@gmail.com>
2022-02-10 13:37:10 -0300

    funcao cadastrarUsuario crida

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ function obterMensagens(){
 function exibe(resposta){
     mensagens = resposta.data
     renderizaMensagens(mensagens)
-
 }
 
 function renderizaMensagens(mensagens){
@@ -23,8 +22,10 @@ function renderizaMensagens(mensagens){
             textoMain.innerHTML += `
             <div class="mensagem__publica">
                 <span>${element.time}</span><span>${element.from}  ${element.text}</span>
-            </div>            
+            </div>           
             `
+            //rolagemAutomatica(rolagem) 
+            
         } else if (element.type === 'private_message'){
 
             textoMain.innerHTML += `
@@ -32,14 +33,37 @@ function renderizaMensagens(mensagens){
                 <span>${element.time}</span><span>${element.from}  ${element.text}</span>
             </div>
             `
+            //rolagemAutomatica(element, 'private__mensagem')
         } else if (element.type === 'status') {
             textoMain.innerHTML += `
             <div class="mensagem__status">
                 <span>${element.time}</span><span>${element.from}  ${element.text}</span>
             </div>
             `
-        }        
-    })
+        }           
+    }) 
 }
 
+function cadastrarUsuario(nome){
+    const pessoa = {
+        name: nome
+    }
+    const promessa = axios.post('https://mock-api.driven.com.br/api/v4/uol/participants',pessoa) 
+}
+
+
+// function rolagem(){
+//     const msg = document.querySelectorAll('.mensagem__publica')
+//     return msg[msg.length -1]
+
+// }
+
+// function rolagemAutomatica(rolagem){
+//     const elementoQueQueroQueApareca = rolagem.querySelector('.mensagem__publica');
+// elementoQueQueroQueApareca.scrollIntoView();
+    
+// }
+
+
+cadastrarUsuario(nome)
 setInterval(obterMensagens, 3000)
